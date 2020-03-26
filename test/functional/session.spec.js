7dbb9d4328e5a3b69b44911c5de98090cec94a26
Thales H <htthales@gmail.com>
2020-03-26 19:11:18 -0300

    Autenticação e testes automatizados finalizado

---
+++ test/functional/session.spec.js
@@ -0,0 +1,31 @@
+const { test, trait } = use('Test/Suite')('Session')
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use ('Factory')
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User');
+
+trait('Test/ApiClient')
+
+test('It should return JWT token when session created', async ({ assert, client })=>{
+  const sessionPayLoad = {
+    email: 'htthales@gmail.com',
+    password:'123456'
+  };
+
+  const user = await Factory
+    .model('App/Models/User')
+    .create(sessionPayLoad);
+
+  const response = await client
+    .post('/sessions')
+    .send({
+      email: 'htthales@gmail.com',
+      password:'123456'
+    })
+    .end()
+
+  response.assertStatus(200);
+  assert.exists(response.body.token)
+});
